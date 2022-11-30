7cf01f9482b6d6550f6fca7823a48aa412cbe269
Andre_Jr <101275023+AndreJr117@users.noreply.github.com>
2022-11-29 21:33:15 -0300

    Add files via upload

---
+++ Att_Lista_Array/ListaFun.java
@@ -0,0 +1,18 @@
+package Att_Lista_Array;
+
+import java.util.ArrayList;
+
+public class ListaFun {
+
+    public static void main(String[] args){
+        ArrayList<String> listaFun = new ArrayList<>(0);
+
+        listaFun.add("Carlos");
+        listaFun.add("Júnior");
+        listaFun.add("André");
+        listaFun.add("jose");
+
+        System.out.println(listaFun);
+
+    }
+}
